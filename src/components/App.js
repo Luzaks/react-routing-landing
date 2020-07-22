6cca9da3a46261b7ae6e09ead9a4554d174b9d75
Luzaks <kanemekanik@gmail.com>
2020-07-22 18:01:30 -0500

    Feat: Add first logic for React Router.

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,49 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import Routes from '../router/routes';
+import { Link, useHistory } from 'react-router-dom';
 import '../styles/App.css';
 
 function App() {
+
+  const history = useHistory();
+  const [path, setPath] = useState('');
+  window.addEventListener('load', () => {
+      if(window.location.pathname === '/contact'){
+        setPath(window.location.pathname)
+      }
+  });
+
+  const checkPath = () => {
+      history.listen((location) => {
+        setPath(location.name);
+      })
+  };
+
+  useEffect(() => {
+    checkPath();
+  }, []);
+
+  const showContact = path;
+  let _contact;
+  if (showContact !== '/contact') {
+    _contact = (<li><Link to="/contact">Contact</Link></li>)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">NewReactRouter</Link>
+            </li>
+          </ul>
+          <ul>
+            {_contact}
+          </ul>
+        </nav>
+        <Routes />
+      </div>
     </div>
   );
 }
